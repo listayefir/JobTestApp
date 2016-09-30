f927b6de31bc38746a69f820ce9894551552e128
Butters <butters@mail.ru>
2016-09-30 13:40:06 +0300

    почти доделан поиск, добавлен скрипт заполнения базы

--- TestApplication.Tests/UnitTest1.cs
+++ TestApplication.Tests/UnitTest1.cs
@@ -1,33 +1,42 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using TestApplication.Concrete;
+using TestApplication.Abstract;
 using TestApplication.Models;
 using System.Linq;
+using Moq;
 
 namespace TestApplication.Tests
 {
-    //[TestClass]
-    //public class UnitTest1
-    //{
-    //    EfLannisterRepository repo;
-    //    LannistersViewModel model; 
-        
-    //    public void Initialize()
-    //    {
-    //        repo = new EfLannisterRepository();
-    //        model = new LannistersViewModel
-    //    {
-    //        Lannisters = repo.Lannisters
-    //    };
-    //}
-    //    [TestMethod]
-    //    public void LannisterExtensionsTest()
-    //    {
-    //        Initialize();
+    [TestClass]
+    public class UnitTest1
+    {
+        ILannisterRepository repo;
+        Mock<ILannisterRepository> mock;
 
-    //        var result = model.Lannisters.ToArray()[4].GetParent(model);
+        public void Initialize()
+        {
+            mock = new Mock<ILannisterRepository>();
+            mock.Setup(m => m.Lannisters).Returns(new Lannister[]{
+                new Lannister { Id=0, Title="A", Created=new DateTime(1995,1,1)},
+                new Lannister { Id=0, Title="B", Created=new DateTime(1990,1,1)},
+                new Lannister { Id=0, Title="C", Created=new DateTime(1999,1,1)},
+                new Lannister { Id=0, Title="D", Created=new DateTime(2000,1,1)},
+            });
+        }
+        [TestMethod]
+        public void DateTimeTest()
+        {
+            Initialize();
+            var from = "11/11/1990";
+            var to = "31/12/1999";
+            var a = DateTime.Parse(from);
+            var b = DateTime.Parse(to);
+            var result = mock.Object.Lannisters.Where(x => x.Created.CompareTo(DateTime.Parse(from)) >= 0 && x.Created.CompareTo(DateTime.Parse(to)) <= 0).ToArray();
 
-    //        Assert.AreEqual("Тайвин Ланнистер", result);
-    //    }
-    //}
+            Assert.AreEqual(2, result.Length);
+            Assert.AreEqual("A", result[0].Title);
+            Assert.AreEqual("C", result[1].Title);
+            
+        }
+    }
 }
--- TestApplication/Controllers/LannistersController.cs
+++ TestApplication/Controllers/LannistersController.cs
@@ -41,19 +41,33 @@ namespace TestApplication.Controllers
                     Created = lannister.Created.Date,
                     Description = lannister.Description
                 };
-              
+
                 model.Lannisters.Add(newLannister);
             }
 
             return View(model);
         }
 
+
         [HttpPost]
-        public ActionResult Table(string orderBy, string thenBy, bool orderByDescending, bool thenByDescending)
+        public ActionResult Table(string orderBy, string thenBy, int? searchById, string searchByTitle, string searchByDescription, string searchByParent,
+            string searchByDateFrom, string searchByDateTo)
         {
             LannistersViewModel model = new LannistersViewModel();
+            if (string.IsNullOrEmpty(searchByDateFrom)) searchByDateFrom = DateTime.MinValue.ToString();
+            if (string.IsNullOrEmpty(searchByDateTo)) searchByDateTo = DateTime.MaxValue.ToString();
+            var a = DateTime.Parse(searchByDateFrom);
+            var b = DateTime.Parse(searchByDateTo);
 
-            foreach (var lannister in _repository.Lannisters)
+
+            var target = _repository.Lannisters
+                .Where(x => x.Id == (searchById ?? x.Id))
+                .Where(x => x.Title.IndexOf(searchByTitle ?? "", StringComparison.OrdinalIgnoreCase) != -1)
+                .Where(x => x.Description != null ? x.Description.IndexOf(searchByDescription ?? "", StringComparison.OrdinalIgnoreCase) != -1 : true)//тут надо что-то сделать
+                .Where(x => String.IsNullOrEmpty(searchByParent) ? true : x.ParentId == _repository.Lannisters.Where(y => y.Title == searchByParent).FirstOrDefault().Id)
+                .Where(x => x.Created.Date.CompareTo(DateTime.Parse(searchByDateFrom))>=0&&x.Created.Date.CompareTo(DateTime.Parse(searchByDateTo))<=0);
+
+            foreach (var lannister in target)
             {
                 var newLannister = new LannisterViewModel
                 {
@@ -67,16 +81,17 @@ namespace TestApplication.Controllers
                     Description = lannister.Description
                 };
 
+
                 model.Lannisters.Add(newLannister);
             }
 
-            var firstPropInfo = typeof(LannisterViewModel).GetProperty(orderBy);
-            var secondtPropInfo = typeof(LannisterViewModel).GetProperty(thenBy);
+            //var firstPropInfo = typeof(LannisterViewModel).GetProperty(orderBy);
+            //var secondtPropInfo = typeof(LannisterViewModel).GetProperty(thenBy);
 
-            if (orderByDescending && thenByDescending) model.Lannisters = model.Lannisters.OrderByDescending(x => firstPropInfo.GetValue(x)).ThenByDescending(x=>secondtPropInfo.GetValue(x)).ToList();
-            else if (orderByDescending && !thenByDescending) model.Lannisters = model.Lannisters.OrderByDescending(x => firstPropInfo.GetValue(x)).ThenBy(x => secondtPropInfo.GetValue(x)).ToList();
-            else if (!orderByDescending && thenByDescending) model.Lannisters = model.Lannisters.OrderBy(x => firstPropInfo.GetValue(x)).ThenByDescending(x => secondtPropInfo.GetValue(x)).ToList();
-            else model.Lannisters = model.Lannisters.OrderBy(x => firstPropInfo.GetValue(x)).ThenBy(x => secondtPropInfo.GetValue(x)).ToList();
+            //if (orderByDescending && thenByDescending) model.Lannisters = model.Lannisters.OrderByDescending(x => firstPropInfo.GetValue(x)).ThenByDescending(x=>secondtPropInfo.GetValue(x)).ToList();
+            //else if (orderByDescending && !thenByDescending) model.Lannisters = model.Lannisters.OrderByDescending(x => firstPropInfo.GetValue(x)).ThenBy(x => secondtPropInfo.GetValue(x)).ToList();
+            //else if (!orderByDescending && thenByDescending) model.Lannisters = model.Lannisters.OrderBy(x => firstPropInfo.GetValue(x)).ThenByDescending(x => secondtPropInfo.GetValue(x)).ToList();
+            //else model.Lannisters = model.Lannisters.OrderBy(x => firstPropInfo.GetValue(x)).ThenBy(x => secondtPropInfo.GetValue(x)).ToList();
 
             return View(model);
         }
